736135835ff7447c3624b5837e256e967cbf6c9a
arthur-ryoo <jene0523@gmail.com>
2020-08-12 23:20:04 -0700

    Add choosing multiple files feature

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { sify } from 'chinese-conv';
 import './App.css';
 
 function App() {
+  const [fileParsingName, setFileParsingName] = useState('');
+  const [downloadUrl, setDownloadUrl] = useState();
+
+  const translateGenerically = async (file) => {
+    setFileParsingName(file.name);
+    const text = await file.text();
+    const translatedText = sify(text);
+    const blob = new Blob([translatedText], {
+      type: 'text/plain',
+    });
+    const downloadUrl = URL.createObjectURL(blob);
+    setDownloadUrl(downloadUrl);
+  };
+
+  const getFileName = () => {
+    const pieces = fileParsingName.split('.');
+    if (!pieces.length) {
+      return fileParsingName;
+    }
+    const extension = pieces[pieces.length - 1];
+    const baseName = fileParsingName.slice(
+      0,
+      fileParsingName.length - extension.length
+    );
+    return `${baseName}${extension}`;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Select a local file on your computer by clicking the choose file
+          button. You will then be provided with a download link with all the
+          text in the file
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input
+          type="file"
+          multiple
+          onChange={(event) => {
+            if (!event.target.files || event.target.files.length === 0) {
+              return;
+            }
+            translateGenerically(event.target.files[0]);
+          }}
+        />
+        <div>
+          {downloadUrl ? (
+            <a href={downloadUrl} download={getFileName()}>
+              {`Download Result - ${getFileName()}`}
+            </a>
+          ) : null}
+        </div>
+      </div>
     </div>
   );
 }
